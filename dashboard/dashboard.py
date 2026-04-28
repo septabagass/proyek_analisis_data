c0fec18d408fef722b326d01ffa16bcef87ecdf0
Septa Bagas Setyawan <septabagass@gmail.com>
2026-04-28 15:30:58 +0700

    Update Program
    ubah keseluruhan

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,8 +4,8 @@ import seaborn as sns
 import streamlit as st
 
 # Load data
-day_df = pd.read_csv("dashboard/day.csv")
-hour_df = pd.read_csv("dashboard/hour.csv")
+day_df = pd.read_csv("day.csv")
+hour_df = pd.read_csv("hour.csv")
 
 day_df['dteday'] = pd.to_datetime(day_df['dteday'])
 
@@ -32,115 +32,109 @@ hour_df['yr_label'] = hour_df['yr'].map({0: 2011, 1: 2012})
 
 
 # Sidebar Filter 
-st.sidebar.title("filter")
+with st.sidebar:
+    st.header("Filter Tanggal")
 
-year = st.sidebar.selectbox('Pilih Tahun', ['Semua', 2011, 2012])
+    min_date = day_df['dteday'].min()
+    max_date = day_df['dteday'].max()
 
-season = st.sidebar.selectbox("Pilih Musim", day_df['season_label'].unique())
+    start_date, end_date = st.date_input(
+        label='Pilih Rentang Tanggal',
+        min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
 
-# filtered_df = day_df[(day_df['season_label'] == season) & (day_df['yr_label'] == year)]
-if year == "Semua":
-    filtered_df = day_df[day_df['season_label'] == season]
-else:
-    filtered_df = day_df[(day_df['season_label'] == season) &(day_df['yr_label'] == year)]
-
+    st.write("Tanggal dipilih:", start_date, "sampai", end_date)
 
 # Header
 st.title("Bike Sharing Dashboard")
 
-st.markdown(f"### Data Tahun {year} - Musim {season.title()}")
+
+filtered_df = day_df[
+    (day_df['dteday'] >= pd.to_datetime(start_date)) &
+    (day_df['dteday'] <= pd.to_datetime(end_date))
+]
 
 # Matrics
 col1, col2, col3 = st.columns(3)
 
-col1.metric("Total Rental", int(filtered_df['cnt'].sum()))
-col2.metric("Rata rata Rental", int(filtered_df['cnt'].mean()))
-col3.metric("Max Rental", int(filtered_df['cnt'].max()))
+total = int(filtered_df['cnt'].sum()) if not filtered_df.empty else 0
+avg = int(filtered_df['cnt'].mean()) if not filtered_df.empty else 0
+max_val = int(filtered_df['cnt'].max()) if not filtered_df.empty else 0
 
-# 1. Tren Harian
-st.subheader("Insight 1: Rush Hour vs Non Rush Hour (2012)")
+with col1:
+    st.metric("Total Rental", f"{total:,}")
 
-data_2012 = hour_df[(hour_df['yr_label'] == 2012) & (hour_df['workingday'] == 1)].copy()
+with col2:
+    st.metric("Average Rental", f"{avg:,}")
+
+with col3:
+    st.metric("Max Rental", f"{max_val:,}")
+
+# 1. RUSH HOUR
+st.subheader("Insight 1: Rush Hour vs Non-Rush Hour")
+
+data_2012 = hour_df[
+    (hour_df['yr_label'] == 2012) & 
+    (hour_df['workingday'] == 1)
+].copy()
 
 def kategori_jam(jam):
     if (7 <= jam <= 9) or (17 <= jam <= 19):
-        return 'rush_hour'
+        return 'Rush Hour'
     else:
-        return 'non_rush_hour'
+        return 'Non Rush Hour'
 
 data_2012['kategori_jam'] = data_2012['hr'].apply(kategori_jam)
 
-rata2 = data_2012.groupby('kategori_jam')['registered'].mean()
+rata2_jam = data_2012.groupby('kategori_jam')['registered'].mean()
 
-if 'rush_hour' in rata2 and 'non_rush_hour' in rata2:
-    rush = rata2['rush_hour']
-    non_rush = rata2['non_rush_hour']
-    persentase = ((rush - non_rush)/non_rush)*100
-    st.metric("Perbedaan (%)", f"{persentase:.2f}%")
+rush = rata2_jam['Rush Hour']
+non_rush = rata2_jam['Non Rush Hour']
+persentase = ((rush - non_rush)/non_rush)*100
 
-# 2. Casual vs Registered
-st.subheader("Insight 2: Dampak Cuaca (Winter)")
+st.metric("Perbedaan (%)", f"{persentase:.2f}%")
+
+fig, ax = plt.subplots()
+
+bars = ax.bar(rata2_jam.index, rata2_jam.values, color=['#90CAF9','#1976D2'])
+
+for bar in bars:
+    yval = bar.get_height()
+    ax.text(bar.get_x() + bar.get_width()/2, yval, f'{yval:.0f}', ha='center')
+
+ax.set_title("Registered Users Comparison")
+st.pyplot(fig)
+
+st.caption("Penggunaan sepeda meningkat saat jam commuting (pagi & sore).")
+
+
+#2 CUACA
+st.subheader("Insight 2: Dampak Cuaca pada Winter")
 
 winter = day_df[day_df['season_label'] == 'winter']
 
-clear = winter[winter['weathersit_label'] == 'Clear']['cnt'].mean()
-light = winter[winter['weathersit_label'] == 'Light Rain']['cnt'].mean()
+weather = winter[winter['weathersit_label'].isin(['Clear', 'Light Rain'])]
 
-penurunan = ((clear - light)/clear)*100
+rata2_cuaca = weather.groupby('weathersit_label')['cnt'].mean()
+
+clear = rata2_cuaca['Clear']
+rain = rata2_cuaca['Light Rain']
+
+penurunan = ((clear - rain)/clear)*100
 
 st.metric("Penurunan (%)", f"{penurunan:.2f}%")
 
-
-# Growth Casual 
-st.subheader("Insight 3: Growth Casual (Weekend)")
-
-weekend = day_df[(day_df['yr_label'] == 2012) & (day_df['workingday'] == 0)]
-
-spring = weekend[weekend['season_label'] == 'spring']['casual'].mean()
-summer = weekend[weekend['season_label'] == 'summer']['casual'].mean()
-
-growth = ((summer - spring)/spring)*100
-
-st.metric("Growth (%)", f"{growth:.2f}%")
-
-
-# Cuaca 
-st.subheader("Pengaruh Cuaca")
-
-weather = filtered_df.groupby('weathersit_label')['cnt'].mean()
-
 fig, ax = plt.subplots()
-weather.plot(kind='bar', ax=ax)
+
+bars = ax.bar(rata2_cuaca.index, rata2_cuaca.values, color=['#90CAF9','#EF5350'])
+
+for bar in bars:
+    yval = bar.get_height()
+    ax.text(bar.get_x() + bar.get_width()/2, yval, f'{yval:.0f}', ha='center')
+
+ax.set_title("Weather Impact (Winter)")
 st.pyplot(fig)
 
-# Pola Jam
-st.subheader("Pola Penyewa Per Jam")
-
-hour_filtered = hour_df[hour_df['yr_label'] == year].groupby('hr')['cnt'].mean()
-
-fig, ax = plt.subplots()
-hour_filtered.plot(ax=ax)
-st.pyplot(fig)
-
-# Workingday
-st.subheader("Workingday vs Weekend")
-
-work = filtered_df.groupby('workingday')['cnt'].mean()
-
-fig, ax = plt.subplots()
-work.plot(kind='bar', ax=ax)
-st.pyplot(fig)
-
-# Heatmap (Advanced)
-st.subheader("Heatmap Jam vs Workingday")
-
-pivot = hour_df.pivot_table(
-    values='cnt',
-    index='hr',
-    columns='workingday',
-    aggfunc='mean'
-)
-
-fig, ax = plt.subplots()
-sns.heatmap(pivot, annot=False)
-st.pyplot(fig)
+st.caption("Cuaca buruk menurunkan minat penggunaan sepeda secara signifikan.")
